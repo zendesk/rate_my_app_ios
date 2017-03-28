b1c35d791c87ef7d9556c3c53fa4d45da51d3ab1
Alan Egan <alan.mac.aodhagain@gmail.com>
2017-03-28 11:33:20 +0100

    adds copywrite and terms

--- RMAReplacementExample/RMAReplacementExample/AppDelegate.swift
+++ RMAReplacementExample/RMAReplacementExample/AppDelegate.swift
@@ -2,8 +2,12 @@
 //  AppDelegate.swift
 //  RMAReplacementExample
 //
-//  Created by Alan Egan on 27/03/2017.
 //  Copyright © 2017 Zendesk. All rights reserved.
+//
+//  By downloading or using Rate My App, You agree to the Zendesk Master
+//  Subscription Agreement https://www.zendesk.com/company/customers-partners/#master-subscription-agreement and Application Developer and API License
+//  Agreement https://www.zendesk.com/company/customers-partners/#application-developer-api-license-agreement and
+//  acknowledge that such terms govern Your use of and access to Rate My App.
 //
 
 import UIKit
